a99daf8e43f82d4859847e00e08ab09b0282c6b4
+ <+>
2023-09-21 20:08:02 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+
+
+newHeader.setAttribute('id','victory');
+
+
+
+newHeader.textContent = 'Kyle is the champion';
+
+
+
+
+
